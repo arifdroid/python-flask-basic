ed4fb25d9ceecfce246585ca850077326d590226
arifdroid <arifdroiddeveloper@gmail.com>
2022-09-03 13:25:10 +0800

    basic python flask with login page html

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html', text="Testing")
+
+
+@auth.route('/logout')
+def logout():
+    return render_template('login.html')
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('sign_up.html')
